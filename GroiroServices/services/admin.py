a8a37bec6e60626eb26c1a4308121a73a709addd
EvgeniiPlus <evgenii.plus@gmail.com>
2023-08-08 16:25:19 +0300

    Запись отчета в БД

--- GroiroServices/services/admin.py
+++ GroiroServices/services/admin.py
@@ -5,9 +5,9 @@ from .models import *
 class ServicesAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'structure', 'price')
     list_display_links = ('id', 'name')
-    search_fields = ('name',)
-    list_editable = ('structure', 'price')
-    list_filter = ('structure', 'name', 'price')
+    search_fields = ('name', )
+    list_editable = ('price',)
+    list_filter = ('structure',)
 
 
 class StructuresAdmin(admin.ModelAdmin):
@@ -15,25 +15,31 @@ class StructuresAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
     search_fields = ('name', 'employee')
     list_editable = ('employee',)
-    list_filter = ('name', 'employee')
 
 
 class RolesAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
     list_display_links = ('id', 'name')
-    search_fields = ('name', )
-    list_filter = ('name', )
+    search_fields = ('name',)
+
 
 
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('id', 'surname', 'name', 'role')
     list_display_links = ('id', 'surname', 'name')
     search_fields = ('surname', 'name')
-    list_editable = ('role', )
-    list_filter = ('role', )
+    list_editable = ('role',)
+    list_filter = ('role',)
+
+
+class ReportsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'service', 'amount', 'sum', 'date_create', 'date_edit')
+    list_display_links = ('id', 'date', 'service')
+    list_filter = ('date', 'service', 'date_create')
 
 
 admin.site.register(Services, ServicesAdmin)
 admin.site.register(Structures, StructuresAdmin)
 admin.site.register(Roles, RolesAdmin)
 admin.site.register(Users, UsersAdmin)
+admin.site.register(Reports, ReportsAdmin)
--- GroiroServices/services/views.py
+++ GroiroServices/services/views.py
@@ -26,6 +26,33 @@ def dailyReport(request):
                 'menu': menu,
                 'title': 'ГрОИРО. Услуги',
                 'services': Services.objects.all(),
+                'structure': Structures.objects.get(id=1),
                 'current_date': datetime.now(),
         }
+
+        if request.method == 'POST':
+                date = request.POST.get('service_date')
+                service_id_list = request.POST.getlist('service_id')
+                service_count_list = request.POST.getlist('service_count')
+                # print(f'{date} \n{service_id_list} \n{service_count_list}')
+
+                for i in range(len(service_id_list)):
+                        if int(service_count_list[i]) != 0:
+                                Reports.objects.create(
+                                        date=date,
+                                        service=Services.objects.get(pk=service_id_list[i]),
+                                        amount=service_count_list[i],
+                                        sum=round(int(service_count_list[i]) *
+                                                  Services.objects.filter(pk=service_id_list[i])[0].price, 2)
+                                )
+                        else:
+                                continue
+
+
+                return redirect('/')
+
+
+
+
+
         return render(request, 'services/DailyReport.html', context)
